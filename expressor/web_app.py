5535d961175f0ea47e0496d875716dbe9473c69b
Mal'Dun Finsterschreck <Stefan Reiterer>
2025-01-04 22:16:59 +0100

    solved the issue with cors

--- expressor/web_app.py
+++ expressor/web_app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template, request, send_file
-import cv2
+from flask import Flask, render_template, request, send_file, g, jsonify
 import av
+import cv2
+import json
 from flask_socketio import SocketIO, emit
 import logging
 import os
 
+# constants and paths
 fpath = os.path.split(__file__)[0]
 TEMPLATE_DIR = "templates"
 STATIC_DIR = os.path.join(TEMPLATE_DIR,"static")
 static_folder = os.path.join(fpath,STATIC_DIR)
 LOG_FNAME = "mia.log"
+CFG_FILE = os.path.join("..","setup_cfg.json")
+with open(CFG_FILE,'r') as jp:
+    cfg = json.load(jp)
+PORT_OPT = "web_port"
+
 
 app = Flask(__name__, static_folder=static_folder)
-socketio = SocketIO(app)
+
+port = int(cfg[PORT_OPT])
+URL_KEY = "url"
+def get_socket_url(port,page="localhost",protocol='ws'):
+    return {URL_KEY:f"{protocol}://{page}:{port}"}
+
+socketio = SocketIO(app,cors_allowed_origins=get_socket_url(port,protocol='http')[URL_KEY])
+
+#g.socket_url = f"ws://127.0.0.1:{port}/"
 
 logger = logging.getLogger(__name__)
 
@@ -39,6 +54,7 @@ def get_video_frames(video_path):
             yield packet.to_image()
     return "Video frames retrieved successfully"
 
+
 # Create a function to handle messages from clients
 @socketio.on('message')
 def handle_message(message):
@@ -48,12 +64,18 @@ def handle_message(message):
         fp.write(message)
     return "Message received successfully"
 
+
+
+#@app.route('/get_socket_url')
+#def get_socket_url_func():
+#    return jsonify({'socket_url': get_socket_url()})
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     logger.info('Rendering index.html template')
     print("Rendering index.html template")
   
-    return render_template('index.html')
+    return render_template('index.html' ,socket_url=get_socket_url(port))
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True,port=port)
